94bf66af4edf6e1448c0b528c0af804638eba866
Connor <c0nnor@airmail.cc>
2024-06-01 22:34:47 +0200

    actually nvm lol

--- src/main.rs
+++ src/main.rs
@@ -37,14 +37,6 @@ async fn main() {
     let data = client.director.lock().unwrap().clip(&parsed_id);
 
     let path = voice::save_clip(&parsed_id, &data);
-    // Get the name of the file that was saved (usually the milliseconds since the Unix epoch) and convert them to human-readable time
-    let path = path.replace("output/", "");
-    let path = path.replace(".wav", "");
-    let human_readable_time = chrono::NaiveDateTime::from_timestamp(
-      path.parse::<i64>().expect("unable to parse path to i64"),
-      0,
-    );
-    let path = human_readable_time.format("%Y-%m-%d %H:%M:%S").to_string();
 
     let client = client.client.clone();
 
